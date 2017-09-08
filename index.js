1e80adb80d1414c0bdf4f5bb2287989e815868c6
Matt <matt.latimer@gmail.com>
2017-09-08 19:23:09 +0000

    coloured a quote about computer science

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var chalk = require("chalk");
 
-var message = "Hello " + chalk.yellow("World");
+var message = 'There are ' + chalk.blueBright.bold('2') + ' ' + chalk.red.underline('hard problems') +
+' in ' + chalk.yellowBright('computer science') + ': ' + chalk.green('cache invalidation') + ', ' +
+chalk.magenta('naming things') + ', and ' + chalk.cyan('off-by-1 errors.');
+
 console.log(message);
